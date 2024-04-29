9572cb75a606093ea616d494610f77c58fcea001
SolipIngen <eventhorizon108@gmail.com>
2024-04-28 22:09:04 -0700

    Trade balance + Hybrid Aquatic compat

---
+++ src/main/java/solipingen/sassot/util/interfaces/mixin/entity/player/PlayerEntityInterface.java
@@ -0,0 +1,15 @@
+package solipingen.sassot.util.interfaces.mixin.entity.player;
+
+import org.jetbrains.annotations.Nullable;
+
+import net.minecraft.item.ItemStack;
+
+
+public interface PlayerEntityInterface {
+    
+
+    @Nullable public ItemStack getFishingRodStack();
+    public void setFishingRodStack(ItemStack itemStack);
+
+
+}
