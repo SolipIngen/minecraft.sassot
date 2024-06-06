0b37b170991c812d0662a011acfa014fb848fcc3
SolipIngen <eventhorizon108@gmail.com>
2024-06-05 19:41:00 -0700

    Smithing tables craftable with any ingot.

--- src/main/java/solipingen/sassot/registry/tag/ModItemTags.java
+++ src/main/java/solipingen/sassot/registry/tag/ModItemTags.java
@@ -25,5 +25,7 @@ public class ModItemTags {
     public static final TagKey<Item> SHIELD_FRAMING_SMITHING_TEMPLATE_INGOTS = TagKey.of(RegistryKeys.ITEM, new Identifier(SpearsAxesSwordsShieldsAndOtherTools.MOD_ID, "shield_framing_smithing_template_ingots"));
 
     public static final TagKey<Item> PIG_TEMPT_ITEMS = TagKey.of(RegistryKeys.ITEM, new Identifier(SpearsAxesSwordsShieldsAndOtherTools.MOD_ID, "pig_tempt_items"));
+
+    public static final TagKey<Item> INGOTS = TagKey.of(RegistryKeys.ITEM, new Identifier(SpearsAxesSwordsShieldsAndOtherTools.MOD_ID, "ingots"));
     
 }
