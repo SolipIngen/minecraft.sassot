1727ddaad5dbb329afd87b6c307de3c537c7c0f9
SolipIngen <eventhorizon108@gmail.com>
2024-06-14 22:39:27 -0700

    Fighter Villager target tags added & implemented.

--- src/main/java/solipingen/sassot/registry/tag/ModEntityTypeTags.java
+++ src/main/java/solipingen/sassot/registry/tag/ModEntityTypeTags.java
@@ -11,5 +11,7 @@ public class ModEntityTypeTags {
 
     public static final TagKey<EntityType<?>> SENSITIVE_TO_LEANING = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(SpearsAxesSwordsShieldsAndOtherTools.MOD_ID, "sensitive_to_leaning"));
 
+    public static final TagKey<EntityType<?>> SPEARMAN_VILLAGER_TARGETS = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(SpearsAxesSwordsShieldsAndOtherTools.MOD_ID, "spearman_villager_targets"));
+    public static final TagKey<EntityType<?>> SWORDSMAN_VILLAGER_TARGETS = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(SpearsAxesSwordsShieldsAndOtherTools.MOD_ID, "swordsman_villager_targets"));
 
 }
